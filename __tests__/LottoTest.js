3908cba21a172dc13867bf38538ec0c22e5b8066
김예안 <kjan0929@naver.com>
2026-03-04 20:09:34 +0900

    test(Lotto): 로또 당첨 번호 및 보너스 번호 비교 테스트 작성
    Co-authored-by: Untae Lee <bigcloud07@users.noreply.github.com>

---
+++ __tests__/LottoTest.js
@@ -0,0 +1,30 @@
+describe("당첨 번호 비교 test", () => {
+  test("일치 개수가 3개일 때", () => {
+    expect(
+      new Lotto([1, 2, 3, 4, 5, 6]).calculateMatchCount([1, 2, 3, 7, 8, 9])
+    ).toBe(3);
+  });
+  test("일치 개수가 4개일 때", () => {
+    expect(
+      new Lotto([1, 2, 3, 4, 5, 6]).calculateMatchCount([1, 2, 3, 4, 8, 9])
+    ).toBe(4);
+  });
+  test("일치 개수가 5개일 때", () => {
+    expect(
+      new Lotto([1, 2, 3, 4, 5, 6]).calculateMatchCount([1, 2, 3, 4, 5, 9])
+    ).toBe(5);
+  });
+  test("일치 개수가 6개일 때", () => {
+    expect(
+      new Lotto([1, 2, 3, 4, 5, 6]).calculateMatchCount([1, 2, 3, 4, 5, 6])
+    ).toBe(6);
+  });
+});
+
+describe("당첨 번호 및 보너스 번호 비교 test", () => {
+  test("5개 일치, 보너스 볼 일치", () => {
+    expect(new Lotto([1, 2, 3, 4, 5, 6]))
+      .calculateMatchCount([1, 2, 3, 4, 5, 11, 6])
+      .toBe(6);
+  });
+});
