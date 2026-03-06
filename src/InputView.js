43f9f1d13daecc17882028ae6211d1382ead8592
김예안 <kjan0929@naver.com>
2026-03-06 15:36:41 +0900

    feat(input): 잘못된 값 입력 시 재입력 기능 추가
    Co-authored-by: Untae Lee <bigcloud07@users.noreply.github.com>

--- src/InputView.js
+++ src/InputView.js
@@ -1,6 +1,5 @@
-import { INPUT_MESSAGES } from "./constants";
-
-const readline = require("readline");
+import { INPUT_MESSAGES } from "./constants.js";
+import readline from "readline";
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -16,19 +15,32 @@ export default class InputView {
     });
   }
 
+  static async readValidInput(query) {
+    try {
+      const input = await InputView.read(query);
+
+      // TODO: 입력값 검증 로직 추가
+
+      return input;
+    } catch (err) {
+      console.log("[ERROR]");
+      return await InputView.readValidInput(query);
+    }
+  }
+
   static close() {
     rl.close();
   }
 
   static async readPurchaseAmount() {
-    return InputView.read(INPUT_MESSAGES.PURCHASE_AMOUNT);
+    return InputView.readValidInput(INPUT_MESSAGES.PURCHASE_AMOUNT);
   }
 
   static readWinningNumbers() {
-    return InputView.read(INPUT_MESSAGES.WINNING_NUMBERS);
+    return InputView.readValidInput(INPUT_MESSAGES.WINNING_NUMBERS);
   }
 
   static readBonusNumbers() {
-    return InputView.read(INPUT_MESSAGES.BONNUS_NUMBERS);
+    return InputView.readValidInput(INPUT_MESSAGES.BONNUS_NUMBERS);
   }
 }
