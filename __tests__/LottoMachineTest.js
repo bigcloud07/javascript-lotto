727b03e2759d2dd381f329662461e07a617e68c8
김예안 <kjan0929@naver.com>
2026-03-06 17:24:50 +0900

    chore(test): 메서드 오타 수정
    Co-authored-by: Untae Lee <bigcloud07@users.noreply.github.com>

--- __tests__/LottoMachineTest.js
+++ __tests__/LottoMachineTest.js
@@ -3,7 +3,7 @@ import LottoMachine from "../src/LottoMachine.js";
 test("로또 발행 test", () => {
   const purchaseAmount = 3000;
   const lottoMachine = new LottoMachine(purchaseAmount);
-  const lottoNumbers = lottoMachine.issueLottos();
+  const lottoNumbers = lottoMachine.issuedLottos();
 
   expect(lottoNumbers.length).toBe(3);
 });
